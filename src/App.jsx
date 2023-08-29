1f2ae89c09714e12b90471d890b8342ffe226ea6
John <ashton.basile@gmail.com>
2023-08-29 16:27:16 +0100

    some practice using promises

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const users = [
+    {
+      name: "Alice",
+      age: 28,
+      country: "USA",
+    },
+    {
+      name: "Bob",
+      age: 35,
+      country: "Canada",
+    },
+    {
+      name: "Charlie",
+      age: 22,
+      country: "Australia",
+    },
+    {
+      name: "David",
+      age: 42,
+      country: "United Kingdom",
+    },
+  ];
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const countriesPromises = users.map((user) =>
+      fetch(`https://restcountries.com/v3.1/name/${user.country}`).then((res) =>
+        res.json()
+      )
+    );
+
+    Promise.all(countriesPromises).then((data) =>
+      setCountries(data.map((country) => country[0]))
+    );
+    console.log(countriesPromises);
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {countries.map((country, index) => (
+        <h2 key={index}>
+          {country.name.common}: {country.region}
+        </h2>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
